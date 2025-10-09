ff39e309b9e8b095e063312712afd35c848c23ba
Angelica Apiacaece <hyuytr@gmail.com>
2025-10-09 15:48:58 +0700

    commit 4

--- NOTE/NoteManager.py
+++ NOTE/NoteManager.py
@@ -1,20 +1,28 @@
 from Note import Note, NoteManager
 import customtkinter as ctk
+from datetime import date, datetime
 
 ctk.set_appearance_mode('light')
 ctk.set_default_color_theme('dark-blue')
+font1 = font = ("Arial", 24, "bold")
+font2 = font = ("Arial", 14)
 
-app = ctk.Ctk()
-app.geometry('500x300')
+app = ctk.CTk()
+app.geometry('500x600')
 app.title("REINA'S DEMO NOTE APP")
 
-notetext = ctk.CtkLabel(app, text = 'NOTE!')
-notetext.pack(pady=20)
+datestuff1 = ctk.CTkLabel(master=app, text_color="#3f8d8f", text=date.today().strftime("%d/%m/%y"))
+datestuff2 = ctk.CTkLabel(master=app, text_color="#3f8d8f", text=datetime.now().strftime("%H:%M:%S"))
+datestuff1.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+datestuff2.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
-entrynote = ctk.CtkEntry(app, )
-ctk.CTkEntry
+notetext = ctk.CTkLabel(app, text = 'NOTE!', font = font1)
+notetext.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+
+noteentry = ctk.CTkEntry(master = app, placeholder_text = "write your note", width = 300, height= 200 )
+noteentry.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
 button = ctk.CTkButton(app, text="Click me", command=lambda: print("Clicked!"))
-button.pack(pady=10)
+button.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
 app.mainloop()
